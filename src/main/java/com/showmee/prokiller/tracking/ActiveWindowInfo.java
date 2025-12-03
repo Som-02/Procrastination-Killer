18da3bf88c0cf22e9103f2d3c9c28bcaad7816ee
Somnath Pal <somnaathpal02@gmail.com>
2025-12-03 21:44:36 +0530

    Update ActiveWindowInfo.java

--- src/main/java/com/showmee/prokiller/tracking/ActiveWindowInfo.java
+++ src/main/java/com/showmee/prokiller/tracking/ActiveWindowInfo.java
@@ -1,9 +1,9 @@
 /**
  * Procrastination Killer
- * Created by: Showmee
+ * Created by: Somnath Pal
  * Year: 2025
  *
- * This project is developed by Showmee. All rights reserved.
+ * This project is developed by Somnath Pal. All rights reserved.
  */
 package com.showmee.prokiller.tracking;
 
@@ -32,4 +32,5 @@ public class ActiveWindowInfo {
                 '}';
     }
 }
+
 
